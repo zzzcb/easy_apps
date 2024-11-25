f981c399cb70e393e2ccd0932e16aa185f785937
zzzcb <1056768935@qq.com>
2024-11-25 20:47:20 +0800

    [add]:增加EasyColor初步实现

--- app/EasyColor/mainwindow.cpp
+++ app/EasyColor/mainwindow.cpp
@@ -1,9 +1,34 @@
 #include "mainwindow.h"
 
+
 MainWindow::MainWindow(QWidget* parent /*= nullptr*/)
 	:QMainWindow(parent)
 {
+	//property browser
+	m_pGroupMng = new QtGroupPropertyManager(this);
+	
+	m_pPointFMngCanEdit = new QtPointFPropertyManager(this);
+	m_pDoubleFac = new QtDoubleSpinBoxFactory(this);
 
+	m_pColorMngCanEdit = new QtColorPropertyManager(this);
+	m_pColorFac = new QtColorEditorFactory(this);
+
+	m_pBrowser = new QtTreePropertyBrowser;
+	m_pBrowser->setHeaderVisible(false);
+	m_pBrowser->setFactoryForManager(m_pPointFMngCanEdit->subDoublePropertyManager(), m_pDoubleFac);
+	m_pBrowser->setFactoryForManager(m_pColorMngCanEdit, m_pColorFac);
+
+
+	//中心窗体
+	auto central = new QWidget;
+	setCentralWidget(central);
+
+	//中心窗体的布局
+	auto mainLayout = new QHBoxLayout(central);
+	mainLayout->addWidget(m_pBrowser);
+
+	m_pBrowser->addProperty(m_pPointFMngCanEdit->addProperty("鼠标位置"));
+	m_pBrowser->addProperty(m_pColorMngCanEdit->addProperty("颜色"));
 }
 
 MainWindow::~MainWindow()
--- app/EasyColor/mainwindow.h
+++ app/EasyColor/mainwindow.h
@@ -2,6 +2,13 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QSplitter>
+
+#include "propertybrowser/qttreepropertybrowser.h"
+#include "propertybrowser/qtpropertymanager.h"
+#include "propertybrowser/qteditorfactory.h"
 
 class MainWindow : public QMainWindow
 {
@@ -9,6 +16,22 @@ public:
 	MainWindow(QWidget* parent = nullptr);
 	~MainWindow();
 
+private:
+
+
+
+
+
+	//property browser
+	QtTreePropertyBrowser* m_pBrowser;
+
+	QtGroupPropertyManager* m_pGroupMng;
+
+	QtPointFPropertyManager* m_pPointFMngCanEdit;
+	QtDoubleSpinBoxFactory* m_pDoubleFac;
+
+	QtColorPropertyManager* m_pColorMngCanEdit;
+	QtColorEditorFactory* m_pColorFac;
 };
 
 #endif
