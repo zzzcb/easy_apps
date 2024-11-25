035f293a929cb033e12692de62278a09d5e8101b
zzzcb <1056768935@qq.com>
2024-11-25 17:10:33 +0800

    [add]:增加EasyColor的图标

--- app/EasyColor/main.cpp
+++ app/EasyColor/main.cpp
@@ -1,12 +1,14 @@
 #include <QApplication>
 
+#include "mainwindow.h"
+
 int main(int argc, char** argv)
 {
 	QApplication app(argc, argv);
 
-
-
+	MainWindow mw;
+	mw.resize(300, 200);
+	mw.show();
 
 	return app.exec();
 }
-
---
+++ app/EasyColor/mainwindow.h
@@ -0,0 +1,14 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+class MainWindow : public QMainWindow
+{
+public:
+	MainWindow(QWidget* parent = nullptr);
+	~MainWindow();
+
+};
+
+#endif
